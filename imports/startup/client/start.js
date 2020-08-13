692391cc87b05ec5c54259eed1ecca77604e16e2
dsutariya <dhruvisutariya49@gmail.com>
2020-08-13 22:09:14 +0530

    hh

--- imports/startup/client/start.js
+++ imports/startup/client/start.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Meteor } from  'meteor/meteor';
-// import { TheLayout } from '../../ui/containers';
-import TheLayout from '../../ui/containers/TheLayout'
-
+import index from '../../ui/containers/index';
 Meteor.startup(() => {
-    render(<TheLayout />, document.getElementById('root'));
+    render(index , document.getElementById('root'));
 });
