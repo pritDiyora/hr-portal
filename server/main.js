7768e7dfb63a2866cf0c6d57d72abb601434e407
Prit Diyorea <radhediyora@gmail.com>
2020-08-31 14:51:35 +0530

    vv

--- server/main.js
+++ server/main.js
@@ -1,13 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/index';
+import '../imports/api/methods/usermethods';
 
 Meteor.startup(() => {
-    process.env.MAIL_URL = "smtp://meteor.email.2014%40gmail.com:P455w0rd2014@smtp.gmail.com:465/";
     // console.log("mailId", process.env);
  
-    Accounts.urls.verifyEmail = function (token) {
-        return Meteor.absoluteUrl('verify-email/' + token);
-    };
+    // Accounts.urls.verifyEmail = function (token) {
+    //     return Meteor.absoluteUrl('verify-email/' + token);
+    // };
     process.env.MAIL_URL = `smtp://superadmi12@gmail.com:prathana@smtp.gmail.com:587/`;
     
     if (Meteor.users.find().count() === 0) {
@@ -25,6 +25,5 @@ Meteor.startup(() => {
         };
         let resultuser = Accounts.createUser(options);
         console.log('reresultuser :: ', resultuser);
-        Accounts.createUser(options);
     }
 });
