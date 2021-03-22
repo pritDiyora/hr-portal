ae7ce5fdad28cfad17a6fec7f77d8b4683e654d9
nits <mavani.nitesh@gmail.com>
2021-03-22 10:52:58 +0530

    bug fixed in server

--- server/main.js
+++ server/main.js
@@ -1,7 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/index';
 import GeneralSetting from '../imports/api/generalsetting/generalsetting';
-
+import User from '../imports/api/user/users';
+import '../imports/api/user/server/method';
+import '../imports/api/user/server/publish';
+import Notification from '../imports/api/notification/notification';
+import Attendance from '../imports/api/attendance/attendance';
+var CronJob = require('cron').CronJob;
 Meteor.startup(async () => {
     process.env.MAIL_URL = `smtp://superadmi12@gmail.com:prathana@smtp.gmail.com:587/`;
 
@@ -36,6 +41,130 @@ Meteor.startup(async () => {
 
         };
         GeneralSetting.insert(general);
+    }
+
+    let officestartTime = GeneralSetting.findOne({ 'isActive': true }, { fields: { 'from': 1 } })
+    let officeStartTime = officestartTime && officestartTime.from
+    var job = new CronJob(`0 15 ${officeStartTime} * * 1-6`, function () {
+        sendNotificationToUserForClockIn();
+        console.log('You will see this message every minutes', new Date());
+    });
+    job.start();
+
+    async function sendNotificationToUserForClockIn() {
+        let users = await User.find({ 'profile.clockStatus': false, 'profile.userType': 'employee' }, { fields: { 'profile.firstName': 1, 'profile.lastName': 1 } }).fetch()
+        let AdminId = User.findOne({ 'profile.userType': 'superadmin' })._id;
+
+        if (users && users.length > 0) {
+            return await users.map((user) => {
+                let fullname = user.profile.firstName + " " + user.profile.lastName
+                let userData = {
+                    title: fullname,
+                    description: 'Reminder for  clockin',
+                    sendId: AdminId,
+                    receiverId: [user._id],
+                    type: 'dashboard',
+                    createdAtDate: new Date(),
+                    createdBy: AdminId,
+                    modifiedBy: AdminId
+                }
+                let sendNoti = Notification.insert(userData);
+                console.log('notificationClockIn :: ', sendNoti);
+            });
+        }
+    }
+
+
+
+    let officeTime = GeneralSetting.findOne({ 'isActive': true }, { fields: { 'to': 1 } })
+    let officetime = officeTime && officeTime.to
+    var job = new CronJob(`0 15 ${officetime} * * 1-6`, function () {
+        sendNotificationToUserForClockOut();
+        console.log('You will see this message every minutes', new Date());
+    });
+    job.start();
+
+    async function sendNotificationToUserForClockOut() {
+        let users = await User.find({ 'profile.clockStatus': true }, { fields: { 'profile.firstName': 1, 'profile.lastName': 1 } }).fetch()
+        let AdminId = User.findOne({ 'profile.userType': 'superadmin' })._id;
+
+        if (users && users.length > 0) {
+            return await users.map((user) => {
+                let fullname = user.profile.firstName + " " + user.profile.lastName
+                let userData = {
+                    title: fullname,
+                    description: 'Reminder for  clockout',
+                    sendId: AdminId,
+                    receiverId: [user._id],
+                    type: 'dashboard',
+                    createdAtDate: new Date(),
+                    createdBy: AdminId,
+                    modifiedBy: AdminId
+                }
+                let sendNoti = Notification.insert(userData);
+                console.log('notification :: ', sendNoti);
+            });
+        }
+    }
+
+
+
+    var job = new CronJob(`0 30 ${officetime} * * 1-6`, function () {
+        autometicallyClockout();
+        console.log('You will see this message every minutes', new Date());
+    });
+    job.start();
+
+    async function autometicallyClockout() {
+        let users = await User.find({ 'profile.clockStatus': true }, { fields: { 'profile.clockStatus': 1 } }).fetch()
+        if (users && users.length > 0) {
+            return await users.map((user) => {
+                let clockStatus = user.profile.clockStatus
+                if (clockStatus == true) {
+                    let updateStatus = User.update({ _id: user._id }, { $set: { 'profile.clockStatus': !user.profile.clockStatus } });
+                    console.log('updateStatus :: ', updateStatus);
+                    let attendanceData = {
+                        userId: user._id,
+                        isCheckIn: !user.profile.clockStatus,
+                        dateTime: new Date(),
+                        date: moment().format("YYYY-MM-DD"),
+                        createdBy: user._id,
+                        modifiedBy: user._id
+                    }
+                    return Attendance.insert(attendanceData);
+                }
+            })
+        }
+    }
+
+    var job = new CronJob(`0 0 10-19/2 * * *`, function () {
+        sendNotificationToUserForReport();
+        console.log('You will see this message every minutes', new Date());
+    });
+    job.start();
+
+    async function sendNotificationToUserForReport() {
+        let users = await User.find({ 'profile.userType': 'employee' }, { fields: { 'profile.firstName': 1, 'profile.lastName': 1 } }).fetch()
+        let AdminId = User.findOne({ 'profile.userType': 'superadmin' })._id;
+
+        if (users && users.length > 0) {
+            return await users.map((user) => {
+                let fullname = user.profile.firstName + " " + user.profile.lastName
+                let userData = {
+                    title: fullname,
+                    description: 'what do you work now?,add your task ',
+                    sendId: AdminId,
+                    receiverId: [user._id],
+                    type: 'task',
+                    createdAtDate: new Date(),
+                    createdBy: AdminId,
+                    modifiedBy: AdminId
+                }
+                let sendNoti = Notification.insert(userData);
+                console.log('notification :: ', sendNoti);
+            });
+        }
+
     }
 
 });
